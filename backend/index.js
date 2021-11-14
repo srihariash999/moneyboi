0970e16b4e27d18b8b425ce44c5cefca3cd10c5a
Moksh Mahajan <mokshmahajan008@gmail.com>
2021-11-14 23:29:56 +0530

    Backend: Basic setup for user registration api

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,16 @@
+const express = require('express');
+const app = express();
+
 const mongoose = require('mongoose');
+
+const users = require('./routes/users');
 
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to mongodb'))
     .catch(err => console.log('Could not connect to mongodb...', err));
+
+app.use(express.json());
+app.use('/api/users', users);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
--- backend/models/user.js
+++ backend/models/user.js
@@ -25,16 +25,16 @@ const userSchema = new mongoose.Schema(
     }
 );
 
-const User = mongoose.Model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
-    const schema = {
+    const schema = Joi.object({
         name: Joi.string().min(3).max(5).required(),
         email: Joi.string().min(5).max(255).required().email(),
         password: Joi.string().min(5).max(1024).required()
-    };
+    });
 
-    return Joi.validate(user, schema);
+    return schema.validate(user);
 }
 
 exports.User = User;
