23d24be38c222f60671f9aabe947532b220eda70
Moksh Mahajan <mokshmahajan008@gmail.com>
2021-11-15 05:50:38 +0530

    Backend: Added loadash for filtering user properties while returning the response

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,24 +1,21 @@
-const {User, validate} = require('../models/user');
+const { User, validate } = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
 
 router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    
-    let user = await User.findOne({email: req.body.email});
+
+    let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered.');
 
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    });
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
 
     await user.save();
 
-    res.send(user);
+    res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
-module.exports = router; 
+module.exports = router;
