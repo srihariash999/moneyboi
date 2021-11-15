ae5f2d6b69fae47a19a5303ac326d2020adf5e60
Moksh Mahajan <mokshmahajan008@gmail.com>
2021-11-15 07:04:40 +0530

    Backend: JWT for  valid login request response

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const Joi = require('joi');
+const jwt = require('jsonwebtoken');
 
 router.post('/', async (req, res) => {
     const { error } = validate(req.body);
@@ -15,7 +16,8 @@ router.post('/', async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if(!validPassword) return res.status(400).send('Invalid email or password.');
 
-    res.send(true);
+    const token = jwt.sign({_id: user._id}, 'jwtPrivateKey');
+    res.send(token);
 });
 
 function validate(req) {
