ed46398a3bb734b33d002678f0a3f439581ba0ba
Srihari Ayapilla <30948010+srihariash999@users.noreply.github.com>
2022-01-15 22:15:25 +0530

    config added to backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,6 +5,12 @@ const router = express.Router();
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 
+
+router.get('/me', auth, async (req, res) => {
+    const user = await User.findById(req.user._id).select('-password');
+    res.send(user);
+});
+
 router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
