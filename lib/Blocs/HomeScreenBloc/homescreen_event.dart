1950ed61eb8cada61de69d6fc558d4907d8b83e8
srihariash999 <srihariash@gmail.com>
2022-01-26 00:58:17 +0530

    add expense ft added

--- lib/Blocs/HomeScreenBloc/homescreen_event.dart
+++ lib/Blocs/HomeScreenBloc/homescreen_event.dart
@@ -9,3 +9,18 @@ class GetExpenseRecordsEvent extends HomeScreenEvent {
     required this.toggleLabel,
   });
 }
+
+class CreateExpenseRecordEvent extends HomeScreenEvent {
+  final String recordDate;
+  final String category;
+  final String remarks;
+  final BuildContext context;
+  final int amount;
+  CreateExpenseRecordEvent({
+    required this.recordDate,
+    required this.category,
+    required this.remarks,
+    required this.amount,
+    required this.context,
+  });
+}
